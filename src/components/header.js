6641b1ca6d8e5378ff978963b7c14c6d0d256642
jpca999 <jpca999@gmail.com>
2017-04-11 16:32:41 -0400

    style working awesome for Header

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,26 @@
 // create a rectangle with text inside it.
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 const Header = () => {
-  const { textStyle } = styles;
+  const { textStyle, viewStyle } = styles;
 
-  return <Text style={textStyle}> Header Albums! </Text>
+  return (
+    <View style={viewStyle}>
+        <Text style={textStyle}>  Header Albums! </Text>
+    </View>
+  );
 };
-
 const styles = {
+  viewStyle:{
+      backgroundColor: '#f5f5ff',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: 90,
+      paddingTop: 15
+  },
   textStyle:{
-    fontSize: 40
-
+    fontSize: 20
   }
 };
 
