18a2cce6477ae1db80c2256c92d209beacb7b9d1
jpca999 <jpca999@gmail.com>
2017-04-11 18:29:27 -0400

    chnaged albumList class to class based function

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,14 +1,16 @@
 // create a rectangle with text inside it.
-import React from 'react';
+import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
 
-const AlbumList = () => {
+class AlbumList extends Component {
+  render () {
     return (
       <View>
         <Text> Album List!!!   </Text>
       </View>
     );
+  }    
 };
 
 export default AlbumList;
