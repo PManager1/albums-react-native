dd2316af28a673c3b004de9506d8cfdcce3e89ce
jpca999 <jpca999@gmail.com>
2017-04-11 14:27:27 -0400

    inital setup running

---
+++ index.ios.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import ReactNative from 'react-native';
+import { Text, AppRegistry } from 'react-native';
+// Create a comp
+const App = function () {
+  return (
+        <Text> s Some Text fuck fuck fuck fuck fuck fuck fuck fuck fuck </Text>
+    );
+};
+// Render the com
+AppRegistry.registerComponent('albums', () => App);
+
+// const App = => (
+//   <Text> Some Text </Text>
+// );
