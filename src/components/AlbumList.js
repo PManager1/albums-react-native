29da81dc09be67a54770c52b3e83c53adf3093fb
jpca999 <jpca999@gmail.com>
2017-04-11 19:02:17 -0400

    console .log workign inside  componentWillMount

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -4,13 +4,22 @@ import { Text, View } from 'react-native';
 
 
 class AlbumList extends Component {
+
+  componentWillMount() {
+
+      console.log( ' compo will mount called - wow ');
+    }
+
+
+
+
   render () {
     return (
       <View>
         <Text> Album List!!!   </Text>
       </View>
     );
-  }    
+  }
 };
 
 export default AlbumList;
