b2f24da29dbe4842f696e2f221807e6a33cbbf90
jpca999 <jpca999@gmail.com>
2017-04-29 14:42:09 -0400

    CardSection is wrapped inside the <Card> component

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -2,12 +2,15 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import Card from './Card';
+import CardSection from './CardSection';
 
 const AlbumDetail = (props) => {
 
 return (
   <Card>
+    <CardSection>
       <Text> {props.album.title}  </Text>
+    </CardSection>
   </Card>
 );
 
