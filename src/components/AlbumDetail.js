587ba6b6ccef13ea27b477786f4f8c04311b564a
jpca999 <jpca999@gmail.com>
2017-04-29 15:33:25 -0400

    45 mid

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -7,19 +7,20 @@ import CardSection from './CardSection';
 const AlbumDetail = ({ album }) => {
 
 const { title, artist, thumbnail_image } = album;
-const { thumbnail_style, headerContentStyle } = styles;
+const { thumbnail_style, headerContentStyle, thumbnailContainerStyle, headerTextStyle } = styles;
 return (
   <Card>
     <CardSection>
 
-    <View>
+    <View style={thumbnailContainerStyle}>
+
       <Image style={thumbnail_style}
              source={{ uri: thumbnail_image }}
       />
     </View>
 
     <View style={headerContentStyle}>
-      <Text> {title}  </Text>
+      <Text style={headerTextStyle}> {title}  </Text>
       <Text> {artist}  </Text>
     </View>
     </CardSection>
@@ -32,9 +33,18 @@ const styles = {
       flexDirection: 'column',
       justifyContent: 'space-around',
     },
+    headerTextStyle: {
+      fontSize: 18
+    },
     thumbnail_style: {
       height: 50,
       width: 50
+    },
+    thumbnailContainerStyle: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginLeft: 10,
+      marginRight: 10
     }
 };
 
