08a4e31ab549e15095d961cdd955110c3c0dad07
jpca999 <jpca999@gmail.com>
2017-04-29 15:24:05 -0400

    44 done- getting images

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,21 +1,40 @@
 
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+
+const { title, artist, thumbnail_image } = album;
 
 return (
   <Card>
     <CardSection>
-      <Text> {props.album.title}  </Text>
-    </CardSection>
 
-    
+    <View>
+      <Image style={styles.thumbnail_style}
+             source={{ uri: thumbnail_image }} />
+    </View>
+
+    <View style={styles.headerContentStyle}>
+      <Text> {title}  </Text>
+      <Text> {artist}  </Text>
+    </View>
+    </CardSection>
   </Card>
 );
+};
 
+const styles = {
+    headerContentStyle: {
+      flexDirection: 'column',
+      justifyContent: 'space-around',
+    },
+    thumbnail_style: {
+      height: 50,
+      width: 50
+    }
 };
 
 export default AlbumDetail;
